6484f505eb3ef38b2290494c03d49f06e146559e
Kubra IRIS <kubra.camp@gmail.com>
2023-10-28 12:32:15 +0100

    serializers rest framework apiview

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+
+class ListRoomsView(APIView):
+
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+
+    def post(self,post):
+        pass
